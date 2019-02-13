dcb8b58215541e4986f1f8786597071ddebc7d9c
caiweishao <419436210@qq.com>
2019-02-13 13:47:37 +0800

    修改面包屑为无状态组件

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import { Breadcrumb } from 'antd';
 
-export default class Breadcrumbs extends Component {
-    render() {
-        const { list } = this.props;
-        return (
-            <Breadcrumb>
-                {
-                    list.map((item, index) => {
-                        return (
-                            item.is_line ? (
-                                <Breadcrumb.Item key={index}><a href={item.line}>{item.name}</a></Breadcrumb.Item>
-                            ) : (
-                                <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>
-                            )
+export default function Breadcrumbs(props) {
+    const { list } = props;
+    return (
+        <Breadcrumb>
+            {
+                list.map((item, index) => {
+                    return (
+                        item.is_line ? (
+                            <Breadcrumb.Item key={index}><a href={item.line}>{item.name}</a></Breadcrumb.Item>
+                        ) : (
+                            <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>
                         )
-                    })
-                }
-            </Breadcrumb>
-        )
-    }
+                    )
+                })
+            }
+        </Breadcrumb>
+    )
 }
