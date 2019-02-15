86dfc0cda146050b2b97af48dd7d1cf9a225b805
caiweishao <419436210@qq.com>
2019-02-15 18:47:14 +0800

    添加打包配置环境test和pro

--- src/fetch/axios.js
+++ src/fetch/axios.js
@@ -1,52 +1,17 @@
 import axios from 'axios';
 import Cookie from 'js-cookie';
 
-// const {
-//   prod,
-//   ws_test,
-//   ws_dev,
-//   http_test,
-//   http_dev,
-//   ws_pre_test,
-//   http_pre_test,
-//   ws_pro,
-//   http_pro
-// } = configPath;
-
-// WS_PATH_MARKET 行情
-// WS_PATH_EXCHANGE 交易
-// KLINE_HISTORY k线历史
-// MARKET_VALUATION_BTC btc估值
-// HTTP_NEWS 新闻爬虫
-
-// 开发环境
-// if (process.env.NODE_ENV === 'development') {
-//   axios.defaults.baseURL = process.env.API_ROOT;
-//   global.WS_PATH_MARKET = ws_dev['market'];
-//   global.WS_PATH_EXCHANGE = ws_dev['exchange'];
-//   global.KLINE_HISTORY = http_dev['history'];
-//   global.MARKET_VALUATION_BTC = http_dev['btc'];
-//   global.HTTP_NEWS = http_dev['news'];
-// }
-
-// 生产环境
-
 // xhr请求带上seetion
 // axios.defaults.withCredentials = true
 
-console.log(process.env.NODE_ENV);
-
+// console.log(process)
 console.log(process.env);
 
-axios.defaults.baseURL = 'http://47.52.66.81:8080';
-// axios.defaults.baseURL = 'https://shop.bttmall.com/shopAPI';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://47.52.66.81:8080' : process.env.REACT_APP_API_URL;
 
-
-// const instance = axios.create();
 axios.interceptors.request.use(function (config) {
     const token = Cookie.get('token');
     if (token) {
-      // config.headers.Authorization = "Bearer " + token
       config.headers.token = token;
     }
     return config;
